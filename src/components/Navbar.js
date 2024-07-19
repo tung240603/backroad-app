f597c196633bf921465bba10b55bd6726da966d5
miss240603 <hoangtung240603@gmail.com>
2024-07-19 07:10:17 +0700

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,52 @@
+import logo from '../images/logo.svg';
+import { socialLinks } from '../data';
+import PageLinks from './PageLinks';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} className="nav-logo" alt="backroads" />
+          <button type="button" className="nav-toggle" id="nav-toggle">
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+        {/* <!-- left this comment on purpose --> */}
+        {/* <ul className="nav-links" id="nav-links">
+          {pageLinks.map(link => {
+            return (
+              <li key={link.id}>
+                <a href={link.href} className="nav-link">
+                  {link.text}
+                </a>
+              </li>
+            );
+          })}
+        </ul> */}
+
+        <PageLinks parentClass="nav-links" itemClass="nav-link" />
+
+        <ul className="nav-icons">
+          {socialLinks.map(link => {
+            const { id, href, icon } = link;
+
+            return (
+              <li key={id}>
+                <a
+                  href={href}
+                  target="_blank"
+                  className="nav-icon"
+                  rel="noreferrer"
+                >
+                  <i className={icon}></i>
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
